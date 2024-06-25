cc7a4f2b6336cb3885c489ae0ad765f4d729af43
Kian Naquines <kjgnaquines@gmail.com>
2024-06-26 02:08:04 +0800

    Update 404.php

--- 404.php
+++ 404.php
@@ -12,7 +12,7 @@
 
 .page_404  img{ width:100%;}
 
-.four_zero_four_bgg{
+.four_zero_four_bg{
  
  background-image: url(https://cdn.dribbble.com/users/285475/screenshots/2083086/dribbble_1.gif);
     height: 400px;
